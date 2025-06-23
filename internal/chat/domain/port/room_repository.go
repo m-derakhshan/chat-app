ddbdd07a97f8fd0f5019ef2df4b71eae23d4d841
m-derakhshan <43886409+m-derakhshan@users.noreply.github.com>
2025-06-23 16:57:42 +0200

    style(adapter-and-domain): update room handler adapter + room repository

--- internal/chat/domain/port/room_repository.go
+++ internal/chat/domain/port/room_repository.go
@@ -1,4 +1,15 @@
 package port
 
+import "media.hiway.chat/internal/chat/domain/model"
+
 type RoomRepository interface {
+	GetAllRooms() ([]*model.RoomModel, error)
+
+	GetRoomByID(id string) (*model.RoomModel, error)
+
+	GetParticipants(roomID string) ([]*model.UserModel, error)
+
+	AddUserToRoom(roomID string, user *model.UserModel) error
+
+	RemoveUserFromRoom(roomID string, userID string) error
 }
