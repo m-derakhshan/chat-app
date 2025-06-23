6aaf99e65c6c882109c61742b346e2f788ce60d5
m-derakhshan <43886409+m-derakhshan@users.noreply.github.com>
2025-06-23 18:40:08 +0200

    feat(main): adding mux + adding main func

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,18 @@
-package cmd
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+
+	log.Println("Starting server on :8080")
+	err := http.ListenAndServe(":8080", router)
+	if err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
+}
