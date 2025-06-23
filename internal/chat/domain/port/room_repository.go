ce476c69ee5300e77fcace6f088ca3ac515a2661
m-derakhshan <43886409+m-derakhshan@users.noreply.github.com>
2025-06-23 17:54:17 +0200

    feat(roomRepository): complete the methods of room repository

--- internal/chat/domain/port/room_repository.go
+++ internal/chat/domain/port/room_repository.go
@@ -3,13 +3,19 @@ package port
 import "media.hiway.chat/internal/chat/domain/model"
 
 type RoomRepository interface {
+	CreateRoom(roomName string) error
+
 	GetAllRooms() ([]*model.RoomModel, error)
 
 	GetRoomByID(id string) (*model.RoomModel, error)
 
-	GetParticipants(roomID string) ([]*model.UserModel, error)
+	UpdateRoom(id string, roomName string) error
+
+	DeleteRoom(id string) error
 
 	AddUserToRoom(roomID string, user *model.UserModel) error
+
+	GetParticipants(roomID string) ([]*model.UserModel, error)
 
 	RemoveUserFromRoom(roomID string, userID string) error
 }
